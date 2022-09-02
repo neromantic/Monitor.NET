ffe5f8e7dc3f8865082c9c73ca35a326fafd00a2
traitor04 <tommyweigel04@gmail.com>
2022-09-02 12:41:18 +0200

    Added Ip Scan

--- Monitor.NET/MainWindow.xaml.cs
+++ Monitor.NET/MainWindow.xaml.cs
@@ -12,6 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Windows.Forms;
+using ListViewItem = System.Windows.Controls.ListViewItem;
+using MessageBox = System.Windows.Forms.MessageBox;
+using static System.Windows.Forms.AxHost;
 
 namespace Monitor.NET
 {
@@ -20,9 +27,68 @@ namespace Monitor.NET
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<cPartsOfIpAddress> lvResult;
+        string sNetAddress;
         public MainWindow()
         {
             InitializeComponent();
+            lvResult = new List<cPartsOfIpAddress>();
+            lvListResult.ItemsSource = lvResult;
+        }
+
+        private void btnScan_Click(object sender, RoutedEventArgs e)
+        {
+            lvResult.Clear();
+            sNetAddress = txtNetworkAddress.Text;
+            Task.Factory.StartNew(new Action(() =>
+            {
+            Ping ping = new Ping();
+                for (int i = 1; i < 255; i++)
+                {
+                    string ip = $"{sNetAddress}.{i}";
+                    PingReply reply = ping.Send(ip, 100);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                            try
+                            {
+                                IPHostEntry host = Dns.GetHostEntry(IPAddress.Parse(ip));
+                                lvResult.Add(new cPartsOfIpAddress() { sIP = ip, sHostName = host.HostName, sState = "Up" });
+                        }
+                            catch
+                            {
+                                //System.Windows.Forms.MessageBox.Show($"Couldn't retrieve hostname from {ip}", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                    }
+                    else
+                    {
+                            lvResult.Add(new cPartsOfIpAddress() { sIP = ip,sHostName = "",sState = "Down" });
+                    }
+                }
+            })); Task.Factory.StartNew(new Action(() =>
+            {
+                for (int i = 2; i < 255; i++)
+                {
+                    string ip = $"{sNetAddress}.{i}";
+                    Ping ping = new Ping();
+                    PingReply reply = ping.Send(ip, 100);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                            try
+                            {
+                                IPHostEntry host = Dns.GetHostEntry(IPAddress.Parse(ip));
+                                lvResult.Add(new cPartsOfIpAddress() { sIP = ip, sHostName = host.HostName, sState = "Up" });
+                            }
+                            catch
+                            {
+                                MessageBox.Show($"Couldn't retrieve hostname from {ip}", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                    }
+                    else
+                    {
+                            lvResult.Add(new cPartsOfIpAddress() {sIP = ip,sHostName = "",sState =  "Down" });
+                    }
+                }
+            }));
         }
     }
 }
