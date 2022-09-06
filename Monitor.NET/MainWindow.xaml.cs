7e33ee4ed01c2d42d6abb2fd5848ee8f74a7322e
neromantic <ne.romantic16@gmail.com>
2022-09-06 14:43:30 +0200

    IpAdresses Range

--- Monitor.NET/MainWindow.xaml.cs
+++ Monitor.NET/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using System.Net.NetworkInformation;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using ListViewItem = System.Windows.Controls.ListViewItem;
 using MessageBox = System.Windows.Forms.MessageBox;
@@ -28,7 +29,12 @@ namespace Monitor.NET
     public partial class MainWindow : Window
     {
         List<cPartsOfIpAddress> lvResult;
-        string sNetAddress;
+        Regex singleIPRegex = new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b");
+        Regex IPWithSubnetRegex = new Regex(@"(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)\s?\/(\b\d{1,3})");
+        Regex IPrangeRegex = new Regex(@"(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b\s?-\s?(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})");
+
+    
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,16 +42,72 @@ namespace Monitor.NET
             lvListResult.ItemsSource = lvResult;
         }
 
+        private static string longToIP(long ip)
+        {
+            return (ip >> 24).ToString() + '.' + ((ip >> 16) & 0xff).ToString() + '.' + ((ip >> 8) & 0xff).ToString() + '.' + (ip & 0xff).ToString();
+        }
+
+        private static long IPToLong(string ipAddress)
+        {
+            IPAddress ip;
+            if (System.Net.IPAddress.TryParse(ipAddress, out ip))
+                return (((long)ip.GetAddressBytes()[0] << 24) | ((int)ip.GetAddressBytes()[1] << 16) | ((int)ip.GetAddressBytes()[2] << 8) | ip.GetAddressBytes()[3]);
+            else return 0;
+        }
+
+        private List<String> getIPAddresses()
+        {
+            List<String> IPs = new List<String>();
+            string input = "";
+
+            System.Windows.Application.Current.Dispatcher.Invoke((Action)delegate {
+                input = txtNetworkAddress.Text;
+            });
+            
+            List<String> ipInputs = input.Split(',').ToList();
+
+            foreach(String ipInput in ipInputs)
+            {
+                
+                if(IPWithSubnetRegex.Match(ipInput).Success)
+                {
+                    Match mathces = IPWithSubnetRegex.Match(ipInput);
+                    string ip = mathces.Groups[0].Captures[0].Value;
+                    int subnet = int.Parse(mathces.Groups[1].Value);
+
+                    long IPStart = MainWindow.IPToLong(ip);
+                    long IPEnd = IPStart | ((1 << (32 - subnet)) - 1);
+
+                    foreach (int currentIp in Enumerable.Range(((int)IPStart), ((int)IPEnd)))
+                    {
+                        IPs.Add(MainWindow.longToIP(currentIp));
+                    }
+                } else if(IPrangeRegex.Match(ipInput).Success)
+                {
+                    Match mathces = IPWithSubnetRegex.Match(ipInput);
+                    long IPStart = MainWindow.IPToLong(mathces.Groups[0].Value);
+                    long IPEnd = MainWindow.IPToLong(mathces.Groups[1].Value);
+
+                    foreach (int currentIp in Enumerable.Range(((int)IPStart), ((int)IPEnd)))
+                    {
+                        IPs.Add(MainWindow.longToIP(currentIp));
+                    }
+                } else if (singleIPRegex.Match(ipInput).Success)
+                {
+                    IPs.Add(ipInput);
+                }
+            }
+            return IPs;
+        }
         private void btnScan_Click(object sender, RoutedEventArgs e)
         {
             lvResult.Clear();
-            sNetAddress = txtNetworkAddress.Text;
+            
             Task.Factory.StartNew(new Action(() =>
             {
             Ping ping = new Ping();
-                for (int i = 1; i < 255; i++)
+                foreach (string ip in getIPAddresses())
                 {
-                    string ip = $"{sNetAddress}.{i}";
                     PingReply reply = ping.Send(ip, 100);
                     if (reply.Status == IPStatus.Success)
                     {
@@ -66,9 +128,8 @@ namespace Monitor.NET
                 }
             })); Task.Factory.StartNew(new Action(() =>
             {
-                for (int i = 2; i < 255; i++)
+                foreach (string ip in getIPAddresses())
                 {
-                    string ip = $"{sNetAddress}.{i}";
                     Ping ping = new Ping();
                     PingReply reply = ping.Send(ip, 100);
                     if (reply.Status == IPStatus.Success)
